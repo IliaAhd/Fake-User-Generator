5007b544ca2c695e9ce368335fecdb03b7027c3f
unknown <vaguaman86@gmail.com>
2024-05-04 11:39:16 +0330

    ver 1.0.0

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,45 @@
+const api = "https://randomuser.me/api/";
 
+const fetchAPI = () => {
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      const userData = data.results[0];
+
+      const { large } = userData.picture;
+      const { first, last } = userData.name;
+      const { country, city, state, street } = userData.location;
+      const { age, date } = userData.dob;
+      const { gender } = userData;
+      const { username } = userData.login;
+      const { email } = userData;
+      const { cell } = userData;
+      const { phone } = userData;
+
+      user_image.setAttribute("src", large);
+      user_address.textContent = `${country}, ${city}, ${state}, ${street.name}`;
+      real_name.textContent = ` ${first} ${last}`;
+      user_age.textContent = `Age: ${age}`;
+      user_gender.textContent = `Gender: ${gender}`;
+      user_birthDate.textContent = `Born: ${calcDate(date)}`;
+
+      user_name.textContent = `@${username}`;
+      user_email.textContent = email;
+
+      phone1.textContent = cell;
+      phone2.textContent = phone;
+
+      contents.classList.remove("opacity-0");
+    })
+    .catch((err) => console.error(err));
+};
+
+const calcDate = (date) => {
+  const convertDate = new Date();
+  const userBirthDate = `${convertDate.getFullYear(date)}/${String(
+    convertDate.getMonth(date)
+  ).padStart(2, 0)}/${String(convertDate.getDay(date)).padStart(2, 0)}`;
+  return userBirthDate;
+};
+
+button.addEventListener("click", fetchAPI);
